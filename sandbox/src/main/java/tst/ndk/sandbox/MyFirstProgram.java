47bbcf5de13f07a10f002a9e7395221cf9adfd06
Nikita D <merryhangedman@gmail.com>
2018-06-20 20:05:10 +0300

    Added some basic functions

--- sandbox/src/main/java/tst/ndk/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/tst/ndk/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package tst.ndk.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Noname");
 
-        System.out.println("Hello, world!");
+        double l = 5;
+        System.out.println("Площадь квадрата со строной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+
+    public static void hello(String somebody){
+          System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }
