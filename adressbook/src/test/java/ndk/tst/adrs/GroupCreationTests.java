dac825fb2df0d2d5da1b5a202c8555ef986f230f
Nikita D <merryhangedman@gmail.com>
2018-06-29 11:17:40 +0300

    Moved Loggin in to BeforeClass

--- adressbook/src/test/java/ndk/tst/adrs/GroupCreationTests.java
+++ adressbook/src/test/java/ndk/tst/adrs/GroupCreationTests.java
@@ -19,16 +19,17 @@ public class GroupCreationTests {
         driver = new FirefoxDriver();
         baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
 
-    @Test
-    public void testGroupCreation() throws Exception {
         driver.get("http://localhost/addressbook/");
         driver.findElement(By.name("user")).clear();
         driver.findElement(By.name("user")).sendKeys("admin");
         driver.findElement(By.name("pass")).clear();
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("//input[@value='Войти']")).click();
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
         driver.findElement(By.linkText("Группы")).click();
         driver.findElement(By.xpath("(//input[@name='new'])[2]")).click();
         driver.findElement(By.name("group_name")).click();
